6adb704091bd9e78cac3fc55ad01fa35a9b87cc3
Ana Fábia Coelho <afcoelho44@gmail.com>
2025-07-08 16:26:20 -0300

    adicioando o prometheus

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { vendaSimplesRoutes } from "./routes/vendaSimplesRoutes";
 import { vendaLeilaoRoutes } from "./routes/vendaLeilaoRoutes";
+import client from "prom-client";
 
 const app = express();
 
@@ -9,6 +10,14 @@ const PORT = 9455;
 app.use(express.json());
 app.use('/venda', vendaSimplesRoutes);
 app.use('/leilao', vendaLeilaoRoutes);
+
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
